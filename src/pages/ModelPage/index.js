e723566ec1d46116289466052d5e3833dd490ed0
Felipe Novaes <f.novaes@focalweb.com.br>
2020-10-04 02:37:24 -0300

    wip

--- src/pages/ModelPage/index.js
+++ src/pages/ModelPage/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import QRCode from "react-qr-code";
 import "@google/model-viewer";
-import { ModelViewer } from "@real2u/react-ar-components";
+import { ModelViewer, ARLink } from "@real2u/react-ar-components";
 import { useParams } from "react-router-dom";
-
-import glb from "../../Resources/Models/plant.glb";
-import usdz from "../../Resources/Models/plant.usdz";
 
 import { Container, QRCodeContainer } from "./styles";
 
@@ -13,6 +10,11 @@ function ModelPage() {
   const { id } = useParams();
 
   console.log(id);
+
+  const glb =
+    "https://cors-anywhere.herokuapp.com/https://dashboardccoast.com/androidModels/Zara_armchair_1.glb";
+  const usdz =
+    "https://cors-anywhere.herokuapp.com/https://dashboardccoast.com/iosModels/Zara_armchair_1.usdz";
 
   return (
     <Container>
@@ -22,6 +24,9 @@ function ModelPage() {
         usdz={usdz}
         popup={false}
       />
+      <ARLink glb={glb} usdz={usdz}>
+        View in 3D
+      </ARLink>
       <QRCodeContainer>
         <QRCode value="http://192.168.0.10:3000/model/165651456" size={100} />
       </QRCodeContainer>
